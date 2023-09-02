3701c0d4ba41d8b574eb5fef6988871e3ec6ccac
AJR <angjunray@live.com>
2023-09-02 15:38:45 +0800

    :sparkles: Vertical countdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,23 @@
 <main>
-	<h1 id="title">COUNTDOWN</h1>
+    <div id="title-container">
+        <div id="title">COUNTDOWN</div>
+    </div>
+    <div>
+        
+    </div>
 </main>
 
 <style>
-	#title{
-		text-align: center;
-		width: 100px;
-		height: 50px;
-		font:bold;
-		transform: rotate(90deg);
-	}
+    #title-container {
+        display: flex;
+        align-items: center;
+        height: 100vh;
+    }
+    #title {
+        font-weight: bold;
+        font-size: 100px;
+        transform-origin: center;
+        transform: translate(-45%) rotate(90deg);
+        text-shadow: 0px 0px 20px rgba(132, 205, 125, 0.764);
+    }
 </style>
