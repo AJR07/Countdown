a9ab089b522fb45f814de4899116ede3e86675f1
AJR07 <angjunray@live.com>
2024-07-06 16:08:10 +0800

    :bug: Fix wrong target path for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,24 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { fileURLToPath, URL } from "node:url";
 
-export default ({ mode }: { mode: string }) => {
-    const env = loadEnv(mode, process.cwd(), "");
-
-    return defineConfig({
-        plugins: [sveltekit()],
-        optimizeDeps: {
-            esbuildOptions: {
-                // Node.js global to browser globalThis
-                define: {
-                    global: "globalThis",
-                },
-                // Enable esbuild polyfill plugins
-                plugins: [
-                    NodeGlobalsPolyfillPlugin({
-                        buffer: true,
-                    }),
-                ],
+export default defineConfig({
+    plugins: [sveltekit()],
+    optimizeDeps: {
+        esbuildOptions: {
+            // Node.js global to browser globalThis
+            define: {
+                global: "globalThis",
             },
+            // Enable esbuild polyfill plugins
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true,
+                }),
+            ],
         },
-        define: {
-            "process.env": env,
-        },
-        resolve: {
-            alias: {
-                "@": fileURLToPath(new URL("./src", import.meta.url)),
-            },
-        },
-    });
-};
+    },
+    build: {
+        target: "ES2022",
+    },
+});
